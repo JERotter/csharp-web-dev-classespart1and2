fd32143915cc9a457ebf49ea4ddd7d7555d7361e
JERotter <49504679+JERotter@users.noreply.github.com>
2022-04-25 15:33:14 -0400

    moving on to 5.5

--- SchoolPractice/Student.cs
+++ SchoolPractice/Student.cs
@@ -7,9 +7,19 @@ namespace SchoolPractice
 {
     public class Student
     {
-        private string Name;
-        private int StudentId;
-        private int NumberOfCredits;
-        private double Gpa;
+
+        private static int nextStudentId = 1;
+        public string Name { get; set; } //Capital letter get/set; autoimplement here//
+        public int StudentId { get; set; }
+        public int NumberOfCredits { get; set; }
+        public double Gpa { get; set; }
+
+        public Student(string name, int studentId, int numberOfCredits, double gpa)
+        {//this. only used with vars local to a method//
+            Name = name;
+            StudentId = studentId;
+            NumberOfCredits = numberOfCredits;
+            Gpa = gpa;
+        }  
     }
 }
